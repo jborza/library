b8164aa96e394ac37e411c0e1241f5d45038bca8
Juraj Borza <jborza@gmail.com>
2025-04-09 18:36:08 +0200

    Delete button now works

---
+++ src/main/kotlin/com/jborza/library/HiddenHttpMethodFilterConfig.kt
@@ -0,0 +1,14 @@
+package com.jborza.library
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.filter.HiddenHttpMethodFilter
+
+@Configuration
+class HiddenHttpMethodFilterConfig {
+
+    @Bean
+    fun hiddenHttpMethodFilter(): HiddenHttpMethodFilter {
+        return HiddenHttpMethodFilter()
+    }
+}
